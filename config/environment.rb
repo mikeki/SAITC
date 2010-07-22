fe588b069a7570f4a32f9428fcf4c919a448bfae
Miguel Cervera <miguel@miguel-laptop.(none)>
2010-07-22 17:29:47 -0500

    MAiler test

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+  config.plugins = [:all]
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
@@ -43,5 +44,3 @@ Rails::Initializer.run do |config|
   config.i18n.default_locale = :es
 
 end
-
-
