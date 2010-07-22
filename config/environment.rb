167bc32684fc48945eba6a091000c0e80b12223b
Miguel Cervera <miguel@miguel-laptop.(none)>
2010-07-22 17:40:35 -0500

    MAiler test

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,8 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
+CONTACT_RECIPIENT = "miguelcererac@gmail.com"
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
