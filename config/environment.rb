86fb02050b856112a80245969831ed50ffd5bacc
Miguel Cervera <miguel@miguel-laptop.(none)>
2010-07-25 21:01:42 -0500

    probando cache

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem 'will_paginate', :version => '2.3.14'
+  config.gem 'memcached-northscale', :lib => 'memcached'
+  require 'memcached'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
