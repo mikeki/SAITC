0b9ef908a8e69ed45589871f04d740154a9ec167
Miguel Cervera <miguel@miguel-laptop.(none)>
2010-07-25 23:32:12 -0500

    un div por ahi

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ config.action_view.cache_template_loading            = true
 # config.cache_store = :mem_cache_store
 
 #HABILITAR AL HACER DEPLOYMENT
-config.cache_store = :mem_cache_store, Memcached::Rails.new
+#config.cache_store = :mem_cache_store, Memcached::Rails.new
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host = "http://assets.example.com"
